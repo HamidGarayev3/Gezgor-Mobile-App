7d6235d350af0ee15874ec2ac9ad384877ae2b78
HamidGarayev3 <hamidgarayev3@gmail.com>
2023-03-07 14:08:03 +0400

    Src and Navigation folders added and packages installed

---
+++ gezgor/src/navigation/stacks/HomeStack.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeStack = () => {
+  return (
+    <View>
+      <Text>HomeStack</Text>
+    </View>
+  )
+}
+
+export default HomeStack
---
+++ gezgor/src/navigation/stacks/ProfileStack.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProfileStack = () => {
+  return (
+    <View>
+      <Text>ProfileStack</Text>
+    </View>
+  )
+}
+
+export default ProfileStack
